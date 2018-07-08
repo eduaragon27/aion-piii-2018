7abc14ba67e0e041156ab0b3973739e146d2ba47
Eduardo <eduaragon27@gmail.com>
2018-07-07 22:38:04 -0500

    7

--- vista/js/mantenimiento.producto.js
+++ vista/js/mantenimiento.producto.js
@@ -74,8 +74,6 @@ $("#btnagregar").click(function(){
     $("#txtPrecio").val("");
     $("#txtColor").val("");
     $("#txtStock").val("0");
-    $("#cboCategoria").val("");
-    $("#cboMarca").val("");
     $("#cboEstado").val("A");
 });
 
@@ -230,7 +228,65 @@ function eliminar(codigo_producto){
         }
     });
 }
+function cargarComboCategoria(p_nombreCombo, p_tipo){
+    $.post
+    (
+	"../controlador/categoria.listar.controller.php"
+    ).done(function(resultado){
+	var datosJSON = resultado;
+	
+        if (datosJSON.estado===200){
+            var html = "";
+            if (p_tipo==="seleccione"){
+                html += '<option value="">Seleccione una categoria</option>';
+            }else{
+                html += '<option value="0">Todas las categorias</option>';
+            }
 
+            
+            $.each(datosJSON.datos, function(i,item) {
+                html += '<option value="'+item.codigo_categoria+'">'+item.nombre_categoria+'</option>';
+            });
+            
+            $(p_nombreCombo).html(html);
+	}else{
+            swal("Mensaje del sistema", resultado , "warning");
+        }
+    }).fail(function(error){
+	var datosJSON = $.parseJSON( error.responseText );
+	swal("Error", datosJSON.mensaje , "error");
+    });
+}
+
+function cargarComboMarca(p_nombreCombo, p_tipo){
+    $.post
+    (
+	"../controlador/marca.listar.controller.php"
+    ).done(function(resultado){
+	var datosJSON = resultado;
+	
+        if (datosJSON.estado===200){
+            var html = "";
+            if (p_tipo==="seleccione"){
+                html += '<option value="">Seleccione una marca</option>';
+            }else{
+                html += '<option value="0">Todos las marcas</option>';
+            }
+
+            
+            $.each(datosJSON.datos, function(i,item) {
+                html += '<option value="'+item.codigo_marca+'">'+item.nombre_marca+'</option>';
+            });
+            
+            $(p_nombreCombo).html(html);
+	}else{
+            swal("Mensaje del sistema", resultado , "warning");
+        }
+    }).fail(function(error){
+	var datosJSON = $.parseJSON( error.responseText );
+	swal("Error", datosJSON.mensaje , "error");
+    });
+}
 
 
    
